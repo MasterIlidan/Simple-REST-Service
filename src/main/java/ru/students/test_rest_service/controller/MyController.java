c5c09a65b1f337155c5545b85c19c93da3700f24
MasterIlidan <segamil2001@gmail.com>
2022-10-11 20:05:23 +0500

    Лабораторная работа №2. Доработка простого REST сервиса.

--- src/main/java/ru/students/test_rest_service/controller/MyController.java
+++ src/main/java/ru/students/test_rest_service/controller/MyController.java
@@ -1,6 +1,8 @@
 package ru.students.test_rest_service.controller;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -8,10 +10,19 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import ru.students.test_rest_service.model.Request;
 import ru.students.test_rest_service.model.Response;
+import ru.students.test_rest_service.service.MyModifyService;
 
 @RestController
 @Slf4j
 public class MyController {
+
+    private final MyModifyService myModifyService;
+
+    @Autowired
+    public MyController(@Qualifier("ModifyErrorMessage") MyModifyService myModifyService) {
+        this.myModifyService = myModifyService;
+    }
+
     @PostMapping(value = "/feedback")
     public ResponseEntity<Response> feedback(@RequestBody Request request) {
         log.info("Request accepted: " + request.toString());
@@ -25,7 +36,9 @@ public class MyController {
                 .errorMessage("")
                 .build();
 
-        log.info("Response Sent: " + response.toString());
+        Response responseAfterModify = myModifyService.modify(response);
+
+        log.info("Response sent: " + response.toString());
         return new ResponseEntity<>(response, HttpStatus.OK);
     }
 }
